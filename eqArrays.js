ed5710b9d7de649287cff298a5947500ddaf40f0
Michel <Michel_motongi@hotmail.com>
2021-11-23 18:52:56 -0500

    added change

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,23 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+module.exports = assertEqual;
+
+const eqArrays = function(first,second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === second[i]) {
+      return true;
     } else {
-      console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+      return false;
     }
-  };
-  module.exports = assertEqual;
-  
-const eqArrays = function(first,second){
-    for(let i = 0;i < first.length;i++){
-        for(let j = 0; j < second.length;j++){
-            if(first[i] === second[j]){
-                return true;
-            }else{
-                return false;
-            }
-        }
-    }
-}
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  }
+};
+eqArrays([1, 2, 3], [3, 2, 1]);
 module.exports = eqArrays;
