8134cddcf009e62e6ad5757700f059a38b619594
Michel <Michel_motongi@hotmail.com>
2021-11-25 17:49:51 -0500

    Take until condition is met

--- takeUntil.js
+++ takeUntil.js
@@ -1,15 +1,15 @@
 const takeUntil = function(array, callback) {
   let newArr = [];
   for (let item of array) {
-    if (callback(item) === false) {
-      newArr.push(item);
-    } else {
+    if (callback(item) === true) {
       break;
+    } else {
+      newArr.push(item);
     }
   } return newArr;
 };
 
-/* TEST
+
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -19,5 +19,4 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-*/
 module.export = takeUntil; 
