7a136e7484634edd1906d0f128ebf6684fbf4ec2
Michel <Michel_motongi@hotmail.com>
2021-11-29 23:21:42 -0500

    Index for project Lotide

---
+++ index.js
@@ -0,0 +1,35 @@
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+
+module.exports = {
+    assertArraysEqual,
+    assertEqual,
+    assertObjectsEqual,
+    countLetters,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    findKeyByValue,
+    head,
+    letterPositions,
+    map,
+    middle,
+    tail,
+    takeUntil,
+    without
+  };
